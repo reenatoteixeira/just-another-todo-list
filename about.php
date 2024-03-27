ccb7e731fd5acd56de8d2dfbeaf7bf6d07b5c943
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-03-27 17:50:44 -0300

    chore: add "About Us" page

---
+++ about.php
@@ -0,0 +1,11 @@
+<?php require_once './templates/header.php' ?>
+
+<main>
+  <div class="flex flex-col justify-center items-center h-screen text-center">
+    <img src="assets/img/barrier.svg" alt="Barrier icon." class="w-52">
+    <h3 class="font-mono text-xl font-bold mt-6">UNDER CONSTRUCTION</h3>
+    <h2>"About us"</h2>
+  </div>
+</main>
+
+<?php require_once './templates/footer.php' ?>
