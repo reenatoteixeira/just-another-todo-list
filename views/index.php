f43aad8375d981cec8b351be961096ae61529c46
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-03-27 16:50:38 -0300

    chore: add semantic HTML

--- views/index.php
+++ views/index.php
@@ -42,10 +42,12 @@
     </nav>
   </header>
 
-  <div class="flex flex-col justify-center items-center h-screen text-center">
-    <img src="assets/img/barrier.svg" alt="Barrier icon." class="w-52">
-    <h3 class="font-mono text-xl font-bold mt-6">UNDER CONSTRUCTION</h3>
-  </div>
+  <main>
+    <div class="flex flex-col justify-center items-center h-screen text-center">
+      <img src="assets/img/barrier.svg" alt="Barrier icon." class="w-52">
+      <h3 class="font-mono text-xl font-bold mt-6">UNDER CONSTRUCTION</h3>
+    </div>
+  </main>
 </body>
 
 </html>
