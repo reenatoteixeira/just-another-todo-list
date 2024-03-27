86a8b431dc6599aff0a9ceb54fea13c3a1b297fb
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-03-27 18:02:34 -0300

    chore: add favicon

--- templates/header.php
+++ templates/header.php
@@ -7,6 +7,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
+  <link rel="shortcut icon" href="/assets/img/favicon.svg" type="image/x-icon">
   <script src="https://cdn.tailwindcss.com"></script>
   <title>JATL - Just Another To-do List</title>
 </head>
