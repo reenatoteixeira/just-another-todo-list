4fd9c423fe29b810539a38afc6f1dc599a841dc7
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-03-27 17:10:30 -0300

    chore: add `404.php` view

---
+++ views/404.php
@@ -0,0 +1,12 @@
+<?php require_once './views/partials/header.php' ?>
+
+<main>
+  <div class="flex flex-col justify-center items-center h-screen text-center">
+    <img src="assets/img/barrier.svg" alt="Barrier icon." class="w-52">
+    <h3 class="font-mono text-xl font-bold mt-6">UNDER CONSTRUCTION</h3>
+    <h2>404 - Page not found</h2>
+    <a href="/" class="underline text-sky-500">Back to home</a>
+  </div>
+</main>
+
+<?php require_once './views/partials/footer.php' ?>
