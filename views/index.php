e1963682fc31ad1f82e0ecce5b5103a8ed796a33
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-03-27 17:10:15 -0300

    chore: add test page identifier

--- views/index.php
+++ views/index.php
@@ -4,6 +4,7 @@
   <div class="flex flex-col justify-center items-center h-screen text-center">
     <img src="assets/img/barrier.svg" alt="Barrier icon." class="w-52">
     <h3 class="font-mono text-xl font-bold mt-6">UNDER CONSTRUCTION</h3>
+    <h2>"To-do tasks"</h2>
   </div>
 </main>
 
