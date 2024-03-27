3a16233a47a10ac5b0eec4d326f4fd59ec2cda85
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-03-27 16:40:15 -0300

    chore: add `routeToView` function

--- config/functions.php
+++ config/functions.php
@@ -14,3 +14,12 @@ function abort(int $code = 404): void
   require_once "./views/404.php";
   die();
 }
+
+function routeToView(string $uri, array $routes): void
+{
+  if (array_key_exists($uri, $routes)) {
+    require_once $routes[$uri];
+  } else {
+    abort();
+  }
+}
