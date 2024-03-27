29188cc7ab707c57cb9f82ff8279ac403f3c178d
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-03-27 17:06:19 -0300

    chore: add desktop navigation menu design

--- views/partials/header.php
+++ views/partials/header.php
@@ -15,25 +15,25 @@
       <a href="/" class="text-white font-bold text-3xl flex items-center">
         <img src="/assets/img/check.svg" alt="List icon" class="text-white h-12"> JATL
       </a>
-      <i class="uil uil-bars text-white text-3xl" id="navToggle"></i>
-      <i class="uil uil-times text-white text-3xl hidden" id="navClose"></i>
+      <i class="min-[767px]:hidden uil uil-bars text-white text-3xl hover:cursor-pointer" id="navToggle"></i>
+      <i class="min-[767px]:hidden uil uil-times text-white text-3xl hover:cursor-pointer hidden" id="navClose"></i>
 
       <div class="max-[767px]:fixed max-[767px]:-top-full max-[767px]:left-0 max-[767px]:w-full max-[767px]:mt-16 max-[767px]:px-10 max-[767px]:py-1 max-[767px]:shadow-md max-[767px]:rounded-b-lg max-[767px]:bg-slate-700 max-[767px]:z-[-1]" id="navMenu">
-        <ul class="text-center">
-          <li class="my-2">
-            <a href="/" class="text-slate-300">
+        <ul class="min-[767px]:flex text-center">
+          <li class="min-[767px]:mx-2 my-2">
+            <a href="/" class="text-slate-300 hover:text-slate-100">
               <i class="uil uil-file"></i> To-do tasks
             </a>
           </li>
           <hr>
-          <li class="my-2">
-            <a href="/" class="text-slate-300">
+          <li class="min-[767px]:mx-2 my-2">
+            <a href="/" class="text-slate-300 hover:text-slate-100">
               <i class="uil uil-file-check"></i> Done tasks
             </a>
           </li>
           <hr>
-          <li class="my-2">
-            <a href="/" class="text-slate-300">
+          <li class="min-[767px]:mx-2 my-2">
+            <a href="/" class="text-slate-300 hover:text-slate-100">
               <i class="uil uil-question-circle"></i> About us
             </a>
           </li>
