a701b15a4cd0176bf9410e279468e59a8ea09330
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-03-27 16:47:03 -0300

    chore: add app header

--- views/index.php
+++ views/index.php
@@ -4,14 +4,23 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
   <script src="https://cdn.tailwindcss.com"></script>
   <title>JATL - Just Another To-do List</title>
 </head>
 
-<body class="box-border bg-slate-900">
+<body>
+  <header class="w-full fixed top-0 left-0 bg-slate-900 shadow-md">
+    <nav class="flex justify-between items-center h-16 max-w-5xl m-auto px-10">
+      <a href="/" class="text-white font-bold text-3xl flex items-center">
+        <img src="/assets/img/check.svg" alt="List icon" class="text-white h-12"> JATL
+      </a>
+    </nav>
+  </header>
+
   <div class="flex flex-col justify-center items-center h-screen text-center">
     <img src="assets/img/barrier.svg" alt="Barrier icon." class="w-52">
-    <h3 class="font-mono text-xl text-white font-bold mt-6">UNDER CONSTRUCTION</h3>
+    <h3 class="font-mono text-xl font-bold mt-6">UNDER CONSTRUCTION</h3>
   </div>
 </body>
 
