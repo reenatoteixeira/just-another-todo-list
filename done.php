869123d95135be271163e937138877a63569f9f2
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-03-27 19:58:55 -0300

    chore: remove "Under construction" page

--- done.php
+++ done.php
@@ -1,11 +1,6 @@
-<?php require_once './templates/header.php' ?>
-
-<main>
-  <div class="flex flex-col justify-center items-center h-screen text-center">
-    <img src="assets/img/barrier.svg" alt="Barrier icon." class="w-52">
-    <h3 class="font-mono text-xl font-bold mt-6">UNDER CONSTRUCTION</h3>
-    <h2>"Done tasks"</h2>
-  </div>
-</main>
+<?php
+$heading = 'Done tasks';
+require_once './templates/header.php';
+?>
 
 <?php require_once './templates/footer.php' ?>
