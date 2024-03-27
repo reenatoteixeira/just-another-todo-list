6fc82348b74724520fa9baa8994acde74eebe6ab
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-03-27 17:28:50 -0300

    fix: fix redirect path

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 $routes = [
   '/' => './views/index.php',
-  '/done' => '/done.php',
+  '/done' => './done.php',
 ];
 
 routeToView($uri, $routes);
