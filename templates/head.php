14608a30833ee0c45a774679e37d643dad5f919d
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-03-27 20:00:06 -0300

    chore: add `head.php` template

---
+++ templates/head.php
@@ -0,0 +1,5 @@
+<section class="bg-white w-full border solid border-b-black">
+  <div class="max-w-5xl m-auto">
+    <h1 class="text-3xl font-bold my-2.5 px-2"><?= $heading ?></h1>
+  </div>
+</section>
