82060d5bc55bcd0470475b27ed29714a2a3c9627
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-03-28 20:54:39 -0300

    chore: add create new task logic

---
+++ config/process.php
@@ -0,0 +1,15 @@
+<?php
+require_once(__DIR__ . '/database.php');
+require_once(__DIR__ . '/../dao/TaskDAO.php');
+
+$taskDAO = new TaskDAO($pdo);
+
+$taskTitle = $_POST['title'];
+$taskDescription = $_POST['description'];
+
+$newTask = new Task();
+$newTask->setTitle($taskTitle);
+$newTask->setDescription($taskDescription);
+
+$taskDAO->create($newTask);
+header('Location: ../index.php');
