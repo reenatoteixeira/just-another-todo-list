af85c6a1e7fc8c9933d8815d1d2541123f2cd85c
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-03-28 20:55:48 -0300

    chore: add `TaskDAO` PDO connection

--- dao/TaskDAO.php
+++ dao/TaskDAO.php
@@ -3,6 +3,13 @@ require_once(__DIR__ . '/../models/Task.php');
 
 class TaskDAO implements TaskInterface
 {
+  private $pdo;
+
+  public function __construct(PDO $pdo)
+  {
+    $this->pdo = $pdo;
+  }
+
   public function create(Task $task)
   {
   }
