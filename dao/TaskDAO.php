42e1eb465671634ed0ed2988f6cb4c12c5b2afbb
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-03-28 20:33:31 -0300

    chore: add `TaskDAO` DAO class structure

---
+++ dao/TaskDAO.php
@@ -0,0 +1,21 @@
+<?php
+require_once(__DIR__ . '/models/Task.php');
+
+class TaskDAO implements TaskInterface
+{
+  public function create(Task $task)
+  {
+  }
+
+  public function read()
+  {
+  }
+
+  public function update(Task $task)
+  {
+  }
+  
+  public function delete($id)
+  {
+  }
+}
