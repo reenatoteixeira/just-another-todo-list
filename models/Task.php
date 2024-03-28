ec28150f7a3fa11afc3128632c5fa57b54d943a3
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-03-28 20:32:47 -0300

    chore: add `Task` class

---
+++ models/Task.php
@@ -0,0 +1,48 @@
+<?php
+class Task
+{
+  private $id;
+  private $title;
+  private $description;
+  private $completed;
+
+  public function getID()
+  {
+    return $this->id;
+  }
+
+  public function setID($id)
+  {
+    $this->id = $id;
+  }
+
+  public function getTitle()
+  {
+    return $this->title;
+  }
+
+  public function setTitle($title)
+  {
+    $this->title = $title;
+  }
+
+  public function getDescription()
+  {
+    return $this->description;
+  }
+
+  public function setDescription($description)
+  {
+    $this->description = $description;
+  }
+
+  public function getCompleted()
+  {
+    return $this->completed;
+  }
+
+  public function setCompleted($completed)
+  {
+    $this->completed = $completed;
+  }
+}
