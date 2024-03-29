af482b8c22d6531b66df47020963444b1259db3e
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-03-29 02:26:33 -0300

    chore: add '404' controller and view

---
+++ controllers/404.php
@@ -0,0 +1,3 @@
+<?php
+$heading = 'Page not found';
+require_once(__DIR__ . '/../views/404.view.php');
---
+++ views/404.view.php
@@ -0,0 +1,3 @@
+<?php require_once(__DIR__ . '/partials/header.php'); ?>
+
+<?php require_once(__DIR__ . '/partials/footer.php'); ?>
