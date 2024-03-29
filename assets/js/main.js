bc28b74819f7ea7a132fef30b37081e4416a9d09
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-03-29 18:06:52 -0300

    chore: update `navMenuToggle` function to `mobileMenuToggle`

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,27 @@
-function navMenuToggle() {
-  const navMenu = document.getElementById("navMenu"),
-    navToggle = document.getElementById("navToggle"),
-    navClose = document.getElementById("navClose");
-
-  if (navToggle) {
-    navToggle.addEventListener("click", () => {
-      navToggle.classList.add("hidden");
-      navClose.classList.remove("hidden");
-      navMenu.classList.remove("max-[767px]:-top-full");
-      navMenu.classList.add("top-0");
-    });
-
-    if (navClose) {
-      navClose.addEventListener("click", () => {
-        navClose.classList.add("hidden");
-        navToggle.classList.remove("hidden");
-        navMenu.classList.remove("top-0");
-        navMenu.classList.add("max-[767px]:-top-full");
-      });
-    }
+function mobileMenuToggle() {
+  const mobileMenu = document.getElementById("mobile-menu"),
+    mobMenuOpen = document.getElementById("mobile-menu-open"),
+    mobMenuClose = document.getElementById("mobile-menu-close");
+  
+  if (mobMenuOpen) {
+    mobMenuOpen.addEventListener("click", () => {
+      mobMenuClose.classList.remove("hidden");
+      mobMenuOpen.classList.add("hidden");
+      mobileMenu.classList.remove("hidden");
+    })
+  }
+  
+  if (mobMenuClose) {
+    mobMenuClose.addEventListener("click", () => {
+      mobMenuOpen.classList.remove("hidden");
+      mobMenuClose.classList.add("hidden");
+      mobileMenu.classList.add("hidden");
+    })
   }
 }
 
 function main() {
-  navMenuToggle();
+  mobileMenuToggle();
 }
 
 main();
