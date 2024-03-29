61b4aa1a4df79d65960462c7c2b2aa9fc4f9da80
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-03-29 02:36:04 -0300

    chore: move router logic to its own file

---
+++ config/router.php
@@ -0,0 +1,11 @@
+<?php
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+$routes = [
+  '/' => './controllers/home.php',
+  '/about' => './controllers/about.php',
+  '/create' => './controllers/create.php',
+  '/done' => './controllers/done.php',
+];
+
+routeToController($uri, $routes);
