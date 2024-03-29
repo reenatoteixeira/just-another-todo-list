9c0f126a36b1f462748e5abd276d3f7f048b74a5
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-03-29 18:26:38 -0300

    chore: update `mobileMenuToggle` to include navMenu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,25 @@
 function mobileMenuToggle() {
-  const mobileMenu = document.getElementById("mobile-menu"),
+  const navMenu = document.getElementById("nav-menu"),
+    mobileMenu = document.getElementById("mobile-menu"),
     mobMenuOpen = document.getElementById("mobile-menu-open"),
     mobMenuClose = document.getElementById("mobile-menu-close");
-  
+
   if (mobMenuOpen) {
     mobMenuOpen.addEventListener("click", () => {
       mobMenuClose.classList.remove("hidden");
       mobMenuOpen.classList.add("hidden");
       mobileMenu.classList.remove("hidden");
-    })
+      navMenu.classList.add("rounded-b-lg");
+    });
   }
-  
+
   if (mobMenuClose) {
     mobMenuClose.addEventListener("click", () => {
       mobMenuOpen.classList.remove("hidden");
       mobMenuClose.classList.add("hidden");
       mobileMenu.classList.add("hidden");
-    })
+      navMenu.classList.remove("rounded-b-lg");
+    });
   }
 }
 
