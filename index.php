6643924b3f1d905b038aaf58b8cee2c563f94476
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-03-29 02:36:40 -0300

    chore: require `router.php`

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
 <?php
 require_once(__DIR__ . '/config/functions.php');
-
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-$routes = [
-  '/' => './controllers/home.php',
-  '/about' => './controllers/about.php',
-  '/create' => './controllers/create.php',
-  '/done' => './controllers/done.php',
-];
-
-routeToController($uri, $routes);
+require_once(__DIR__ . '/config/router.php');
