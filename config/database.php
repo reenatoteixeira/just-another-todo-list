738445b8d9f4ed895fe935a92f710e93d327b810
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-03-28 21:23:08 -0300

    chore: setup stage environment

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
 $config = [
-  'host' => 'localhost',
-  'username' => 'root',
-  'password' => '123456',
-  'database' => 'jatl_db'
+  'host' => 'just-another-todo-list_stage-mysql',
+  'database' => 'just-another-todo-list',
+  'username' => 'mysql',
+  'password' => '9425c7846d2f4a00b4e4',
 ];
 
 $pdo = new PDO("mysql:host={$config['host']};dbname={$config['database']}", $config['username'], $config['password']);
