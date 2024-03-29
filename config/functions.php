67fa4957fd7a30ad0e106f6d890a143dba4461b5
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-03-29 02:28:45 -0300

    chore: add `abort` function

--- config/functions.php
+++ config/functions.php
@@ -3,3 +3,10 @@ function checkUrl($url)
 {
   return $_SERVER['REQUEST_URI'] === $url;
 }
+
+function abort($code = 404)
+{
+  http_response_code($code);
+  require_once(__DIR__ . "/controllers/{$code}.php");
+  die();
+}
