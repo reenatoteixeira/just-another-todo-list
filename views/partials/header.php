0d539308290128cc6bb8be19249d0827d069007a
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-03-29 18:25:37 -0300

    chore: make header fixed

--- views/partials/header.php
+++ views/partials/header.php
@@ -12,7 +12,7 @@
 
 <body class="h-full">
   <div class="min-h-full">
-    <nav class="bg-gray-800">
+    <nav class="bg-gray-800 fixed top-0 w-full z-10">
       <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div class="flex h-16 items-center justify-between">
           <div class="flex items-center">
