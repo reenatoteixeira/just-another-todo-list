e8382485dca1559169b4a37a4205a44573fbe478
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-03-28 21:11:31 -0300

    chore: add render tasks from database feature

--- index.php
+++ index.php
@@ -1,19 +1,26 @@
 <?php
 $heading = 'To-do tasks';
-require_once(__DIR__ . '/./templates/header.php');
+require_once(__DIR__ . '/config/database.php');
+require_once(__DIR__ . '/dao/TaskDAO.php');
+require_once(__DIR__ . '/templates/header.php');
+
+$taskDAO = new TaskDAO($pdo);
+$tasks = $taskDAO->read();
 ?>
 
 <section class="max-w-5xl m-auto grid grid-cols-3 max-[767px]:grid-cols-1">
-  <div class="bg-amber-200 m-2 p-4 shadow-md">
-    <h2 class="font-bold">Task #1</h2>
-    <hr class="border-black my-2">
-    <p>Task #1 description. Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure sed dicta quam vero laborum voluptatum facilis! Delectus porro asperiores debitis, natus, incidunt vel odit vitae quisquam, quo pariatur nisi possimus tests.</p>
-    <div class="mt-2">
-      <button class="bg-amber-300 hover:bg-amber-400 font-bold py-2 px-4 rounded"><i class="uil uil-check-circle"></i></button>
-      <button class="bg-amber-300 hover:bg-amber-400 font-bold py-2 px-4 rounded"><i class="uil uil-edit"></i></button>
-      <button class="bg-amber-300 hover:bg-amber-400 font-bold py-2 px-4 rounded"><i class="uil uil-trash-alt"></i></button>
+  <?php foreach ($tasks as $task) : ?>
+    <div class="bg-amber-200 m-2 p-4 shadow-md">
+      <h2 class="font-bold"><?= $task->getTitle() ?></h2>
+      <hr class="border-black my-2">
+      <p><?= $task->getDescription() ?></p>
+      <div class="mt-2">
+        <button class="bg-amber-300 hover:bg-amber-400 font-bold py-2 px-4 rounded"><i class="uil uil-check-circle"></i></button>
+        <button class="bg-amber-300 hover:bg-amber-400 font-bold py-2 px-4 rounded"><i class="uil uil-edit"></i></button>
+        <button class="bg-amber-300 hover:bg-amber-400 font-bold py-2 px-4 rounded"><i class="uil uil-trash-alt"></i></button>
+      </div>
     </div>
-  </div>
+  <?php endforeach; ?>
 
   <div class="fixed bottom-2 right-2">
     <a href="/create.php">
