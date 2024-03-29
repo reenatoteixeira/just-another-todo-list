7a72d09db037ca18200992e6b2c439045ca368de
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-03-29 18:05:44 -0300

    chore: add Tailwind UI header

--- views/partials/header.php
+++ views/partials/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-full bg-gray-100">
 
 <head>
   <meta charset="UTF-8">
@@ -10,38 +10,50 @@
   <title>JATL - Just Another To-do List</title>
 </head>
 
-<body class="bg-slate-100">
-  <header class="w-full fixed top-0 left-0 bg-slate-900 shadow-md">
-    <nav class="flex justify-between items-center h-16 max-w-5xl m-auto px-10">
-      <a href="/" class="text-white font-bold text-3xl flex items-center">
-        <img src="/assets/img/check.svg" alt="List icon" class="text-white h-12"> JATL
-      </a>
-      <i class="min-[767px]:hidden uil uil-bars text-white text-3xl hover:cursor-pointer" id="navToggle"></i>
-      <i class="min-[767px]:hidden uil uil-times text-white text-3xl hover:cursor-pointer hidden" id="navClose"></i>
+<body class="h-full">
+  <div class="min-h-full">
+    <nav class="bg-gray-800">
+      <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div class="flex h-16 items-center justify-between">
+          <div class="flex items-center">
+            <div class="flex-shrink-0">
+              <a href="/">
+                <img class="h-8 w-8" src="/assets/img/check.svg" alt="JATL logo">
+              </a>
+            </div>
+            <div class="hidden md:block">
+              <div class="ml-10 flex items-baseline space-x-4">
+                <a href="/" class="<?= checkUrl('/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium" aria-current="page">To-do</a>
+                <a href="/done" class="<?= checkUrl('/done') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium">Done</a>
+                <a href="/about" class="<?= checkUrl('/about') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium">About us</a>
+              </div>
+            </div>
+          </div>
 
-      <div class="max-[767px]:fixed max-[767px]:-top-full max-[767px]:left-0 max-[767px]:w-full max-[767px]:mt-16 max-[767px]:px-10 max-[767px]:py-1 max-[767px]:shadow-md max-[767px]:rounded-b-lg max-[767px]:bg-slate-700" id="navMenu">
-        <ul class="min-[767px]:flex text-center">
-          <li class="min-[767px]:mx-2 my-2">
-            <a href="/" class="<?= checkUrl('/') ? 'text-white font-bold' : 'text-slate-300' ?> hover:text-slate-100">
-              <i class="uil uil-file"></i> To-do tasks
-            </a>
-          </li>
-          <hr>
-          <li class="min-[767px]:mx-2 my-2">
-            <a href="/done" class="<?= checkUrl('/done') ? 'text-white font-bold' : 'text-slate-300' ?> hover:text-slate-100">
-              <i class="uil uil-file-check"></i> Done tasks
-            </a>
-          </li>
-          <hr>
-          <li class="min-[767px]:mx-2 my-2">
-            <a href="/about" class="<?= checkUrl('/about') ? 'text-white font-bold' : 'text-slate-300' ?> hover:text-slate-100">
-              <i class="uil uil-question-circle"></i> About us
-            </a>
-          </li>
-        </ul>
+          <div class="-mr-2 flex md:hidden">
+            <!-- Mobile menu button -->
+            <button type="button" class="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" aria-controls="mobile-menu" aria-expanded="false">
+              <!-- Menu open: "hidden", Menu closed: "block" -->
+              <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" id="mobile-menu-open">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+              </svg>
+              <!-- Menu open: "block", Menu closed: "hidden" -->
+              <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" id="mobile-menu-close">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <!-- Mobile menu, show/hide based on menu state. -->
+      <div class="hidden md:hidden" id="mobile-menu">
+        <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
+          <a href="/" class="<?= checkUrl('/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> block rounded-md px-3 py-2 text-base font-medium" aria-current="page">To-do</a>
+          <a href="/done" class="<?= checkUrl('/done') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> block rounded-md px-3 py-2 text-base font-medium">Done</a>
+          <a href="/about" class="<?= checkUrl('/about') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> block rounded-md px-3 py-2 text-base font-medium">About us</a>
+        </div>
       </div>
     </nav>
 
     <?php require_once(__DIR__ . '/heading.php') ?>
-  </header>
-  <main class="mt-32">
