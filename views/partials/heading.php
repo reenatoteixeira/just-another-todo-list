38f1dd16d55b4448a654c029ed26163149a1ef7d
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-03-29 18:26:17 -0300

    chore: make heading fixed

--- views/partials/heading.php
+++ views/partials/heading.php
@@ -1,8 +1,8 @@
-<header class="bg-white shadow">
+<header class="bg-white shadow fixed top-16 w-full z-0">
   <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
     <h1 class="text-3xl font-bold tracking-tight text-gray-900"><?= $heading ?></h1>
   </div>
 </header>
 
-<main>
+<main class="pt-36">
   <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
