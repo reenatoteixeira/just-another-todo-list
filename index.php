2fd0f3878994757a8be6679fe39656f70d8b441a
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-03-29 02:22:16 -0300

    chore: add `routes` array

--- index.php
+++ index.php
@@ -3,6 +3,13 @@ require_once(__DIR__ . '/config/functions.php');
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
+$routes = [
+  '/' => './controllers/home.php',
+  '/about' => './controllers/about.php',
+  '/create' => './controllers/create.php',
+  '/done' => './controllers/done.php',
+];
+
 if ($uri === '/') {
   require_once(__DIR__ . '/./controllers/home.php');
 }
