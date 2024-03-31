b93419c089ddd9cf9e71cab17f7a40ecd209d654
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-03-31 12:53:31 -0300

    chore: move warning message back to home page

--- views/create.view.php
+++ views/create.view.php
@@ -1,12 +1,4 @@
 <?php require_once(__DIR__ . '/partials/header.php'); ?>
-<div class="flex bg-red-100 text-red-800 rounded-lg p-4 mx-2" id="warning-msg">
-  <div class="mr-4">
-    <h2><strong>WARNING!</strong></h2>
-    <p>This is a test environment. Be aware that creating a new task will make it public to whoever has access to this website. There's no way to DELETE a created task yet. We're working on future improvements, <a href="https://github.com/reenatoteixeira/just-another-todo-list/tree/stage" class="underline">click here</a> to check our GitHub repository. Thanks!</p>
-  </div>
-  <i class="uil uil-times text-xl hover:cursor-pointer" id="warning-close"></i>
-</div>
-
 <section>
   <form action="/config/process.php" method="POST" class="max-[767px]:mx-4 max-w-2xl m-auto grid grid-cols-1 gap-4">
     <div>
--- views/home.view.php
+++ views/home.view.php
@@ -7,6 +7,14 @@ $taskDAO = new TaskDAO($pdo);
 $tasks = $taskDAO->read();
 ?>
 
+<div class="flex bg-red-100 text-red-800 rounded-lg p-4 m-2" id="warning-msg">
+  <div class="mr-4">
+    <h2><strong>WARNING!</strong></h2>
+    <p>This is a test environment. Be aware that creating a new task will make it public to whoever has access to this website. There's no way to DELETE a created task yet. We're working on future improvements, <a href="https://github.com/reenatoteixeira/just-another-todo-list/tree/stage" class="underline">click here</a> to check our GitHub repository. Thanks!</p>
+  </div>
+  <i class="uil uil-times text-xl hover:cursor-pointer" id="warning-close"></i>
+</div>
+
 <section class="m-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
   <?php foreach ($tasks as $task) : ?>
     <div class="bg-amber-200 p-4 shadow-md overflow-hidden">
