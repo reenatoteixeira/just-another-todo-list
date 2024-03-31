a023ac2cd11bed0220188d5b6d12250794a6b700
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-03-31 13:06:00 -0300

    chore: delete unused route

--- config/router.php
+++ config/router.php
@@ -4,7 +4,6 @@ $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $routes = [
   '/' => './controllers/home.php',
   '/about' => './controllers/about.php',
-  '/create' => './controllers/create.php',
   '/done' => './controllers/done.php',
 ];
 
