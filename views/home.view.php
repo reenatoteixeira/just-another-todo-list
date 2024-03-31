dd603f9094d4cae8e978c8094f4b657222c6fbdf
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-03-31 13:18:36 -0300

    chore: make "Description" field non-resizable

--- views/home.view.php
+++ views/home.view.php
@@ -32,7 +32,7 @@ $tasks = $taskDAO->read();
       </div>
       <div class="px-4 my-2">
         <label for="description" class="block text-sm font-medium leading-6 text-gray-900 md:text-base">Description</label>
-        <textarea name="description" id="description" rows="4" maxlength="255" class="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 text-sm placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 md:text-base"></textarea>
+        <textarea name="description" id="description" rows="4" maxlength="255" class="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 text-sm placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 md:text-base resize-none"></textarea>
       </div>
       <div class="bg-gray-50 px-4 rounded-b-lg">
         <button type="submit" class="mt-3 inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 sm:my-3 sm:w-auto md:text-base">Create task</button>
