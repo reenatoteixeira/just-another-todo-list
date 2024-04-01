da51b82c72dd761007917c896e5c5d1ebd22312f
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-04-01 09:10:00 -0300

    chore: update `mobileMenuToggle` function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,13 @@
 function mobileMenuToggle() {
-  const navMenu = document.getElementById("nav-menu"),
-    mobileMenu = document.getElementById("mobile-menu"),
-    mobMenuOpen = document.getElementById("mobile-menu-open"),
-    mobMenuClose = document.getElementById("mobile-menu-close");
+  const mobMenuOpen = document.getElementById("mobile-menu-open"),
+    mobMenuClose = document.getElementById("mobile-menu-close"),
+    mobMenu = document.getElementById("mobile-menu"),
+    navMenu = document.getElementById("nav-menu");
 
-  if (mobMenuOpen) {
-    mobMenuOpen.addEventListener("click", () => {
-      mobMenuClose.classList.remove("hidden");
-      mobMenuOpen.classList.add("hidden");
-      mobileMenu.classList.remove("hidden");
-      navMenu.classList.add("rounded-b-lg");
-    });
-  }
-
-  if (mobMenuClose) {
-    mobMenuClose.addEventListener("click", () => {
-      mobMenuOpen.classList.remove("hidden");
-      mobMenuClose.classList.add("hidden");
-      mobileMenu.classList.add("hidden");
-      navMenu.classList.remove("rounded-b-lg");
-    });
-  }
+  mobMenuOpen.classList.toggle("hidden");
+  mobMenuClose.classList.toggle("hidden");
+  mobMenu.classList.toggle("hidden");
+  navMenu.classList.toggle("rounded-b-lg");
 }
 
 function closeWarning() {
@@ -57,7 +44,6 @@ function closeNewTaskModalOnClick() {
 }
 
 function main() {
-  mobileMenuToggle();
   closeNewTaskModalOnClick();
 }
 
