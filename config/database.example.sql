297b6d79b96bf0f78c09e6192cd4bb135eb4788b
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-04-02 12:34:06 -0300

    chore: add database setup guide file

---
+++ config/database.example.sql
@@ -0,0 +1,13 @@
+-- Create your own database (change 'database_name' to the desired name):
+CREATE DATABASE database_name;
+
+-- Select your database (change 'database_name' to your database name):
+USE DATABASE database_name;
+
+-- Create your own table (change 'table_name' to the desired name):
+CREATE TABLE table_name (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(125) NOT NULL,
+    description VARCHAR(255),
+    completed INT(1) NOT NULL DEFAULT 0,
+);
