f41522cbfcdf302c9b8a4672443ee258cb971826
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-04-03 10:42:59 -0300

    fix: fix minor heading style bug

--- views/partials/heading.php
+++ views/partials/heading.php
@@ -1,7 +1,7 @@
 <header class="bg-white shadow fixed top-16 w-full z-0">
   <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 flex justify-between">
     <div>
-      <h1 class="text-3xl font-bold tracking-tight text-gray-900"><?= $heading ?></h1>
+      <h1 class="text-4xl font-bold tracking-tight text-gray-900"><?= $heading ?></h1>
     </div>
     <?php if ($heading === 'To-do') : ?>
       <div class="fixed bottom-2 right-4 sm:static">
