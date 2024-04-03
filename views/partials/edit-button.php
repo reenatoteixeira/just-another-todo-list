091c0904412a7959165b05885219639b0a6cbe0f
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-04-03 17:36:55 -0300

    feat: update "Edit" button to open "Edit task" modal
    This commit updates the "Edit" button making it open\n
    the "Edit task" modal and recover the desired task\n
    information to be shown inside the related fields.

--- views/partials/edit-button.php
+++ views/partials/edit-button.php
@@ -1,7 +1,3 @@
-<form action="/config/process.php" method="post">
-  <input type="hidden" name="type" value="edit">
-  <input type="hidden" name="id" value="<?= $task->getID() ?>">
-  <button disabled type="submit" class="ml-2 rounded-md bg-amber-600 px-3 py-2 text-white shadow-sm hover:bg-amber-500 disabled:bg-amber-500 disabled:text-amber-200">
-    <i class="uil uil-edit"></i>
-  </button>
-</form>
+<button onclick="openEditTaskModal(this)" class="ml-2 rounded-md bg-amber-600 px-3 py-2 text-white shadow-sm hover:bg-amber-500 disabled:bg-amber-500 disabled:text-amber-200" task-id="<?= $task->getID() ?>" task-title="<?= $task->getTitle() ?>" task-description="<?= $task->getDescription() ?>">
+  <i class="uil uil-edit"></i>
+</button>
