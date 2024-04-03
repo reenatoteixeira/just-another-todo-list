533cd42c0fe09ebd82a582cd124a8de945c4377d
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-04-03 09:58:48 -0300

    chore: add page heading to the delete button form

--- views/partials/delete-button.php
+++ views/partials/delete-button.php
@@ -2,6 +2,7 @@
   <input type="hidden" name="type" value="delete">
   <input type="hidden" name="id" value="<?= $task->getID() ?>">
   <input type="hidden" name="deleted" value="1">
+  <input type="hidden" name="heading" value="<?= $heading ?>">
   <button type="submit" class="ml-2 rounded-md bg-amber-600 px-3 py-2 text-white shadow-sm hover:bg-amber-500 disabled:bg-amber-500 disabled:text-amber-200">
     <i class="uil uil-trash-alt"></i>
   </button>
