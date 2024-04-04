fa6359fd44e18f333037d6bb5d9f86387fdf9cdd
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-04-04 19:09:13 -0300

    refactor: update "Edit" button to fit new function
    This commit updates the "Edit" button to execute the\n
    new `openModal` function.

--- views/partials/edit.button.php
+++ views/partials/edit.button.php
@@ -1,3 +1,3 @@
-<button onclick="openEditTaskModal(this)" class="ml-2 rounded-md bg-amber-600 px-3 py-2 text-white text-sm font-semibold shadow-sm sm:text-base hover:bg-amber-500 disabled:bg-amber-500 disabled:text-amber-200" task-id="<?= $task->getID() ?>" task-title="<?= $task->getTitle() ?>" task-description="<?= $task->getDescription() ?>">
+<button onclick="openModal(this)" modal-id="edit-task-modal" task-id="<?= $task->getID() ?>" task-title="<?= $task->getTitle() ?>" task-description="<?= $task->getDescription() ?>" class="ml-2 rounded-md bg-amber-600 px-3 py-2 text-white text-sm font-semibold shadow-sm sm:text-base hover:bg-amber-500 disabled:bg-amber-500 disabled:text-amber-200">
   <i class="uil uil-edit"></i> Edit
 </button>
