8d85ae9f4c086044c4f98b56747c7d9f21bd1583
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-04-04 09:59:45 -0300

    refactor: remove unused code from "Mark as done" button
    This commit removes the unused code from the\n
    "Mark as done" button without changing its functionality.

--- views/partials/mark-done.button.php
+++ views/partials/mark-done.button.php
@@ -2,10 +2,7 @@
   <input type="hidden" name="type" value="complete">
   <input type="hidden" name="id" value="<?= $task->getID() ?>">
   <input type="hidden" name="completed" value="1">
-  <button type="submit" class="rounded-md bg-amber-600 px-3 py-2 text-white shadow-sm hover:bg-amber-500 disabled:bg-amber-500 disabled:text-amber-200">
-    <i class="uil uil-check-circle"></i>
-    <span class="font-semibold">
-      Mark as done
-    </span>
+  <button type="submit" class="rounded-md bg-amber-600 px-3 py-2 text-white font-semibold shadow-sm hover:bg-amber-500 disabled:bg-amber-500 disabled:text-amber-200">
+    <i class="uil uil-check-circle"></i> Mark as done
   </button>
 </form>
