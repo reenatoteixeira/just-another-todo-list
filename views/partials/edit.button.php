5c5692ff24a1a3d87006933c56523fdf0286d9cd
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-04-04 09:59:10 -0300

    refactor: update "Edit" button component
    This commit updates the "Edit" button component\n
    to match the new design of the application.

--- views/partials/edit.button.php
+++ views/partials/edit.button.php
@@ -1,3 +1,3 @@
-<button onclick="openEditTaskModal(this)" class="ml-2 rounded-md bg-amber-600 px-3 py-2 text-white shadow-sm hover:bg-amber-500 disabled:bg-amber-500 disabled:text-amber-200" task-id="<?= $task->getID() ?>" task-title="<?= $task->getTitle() ?>" task-description="<?= $task->getDescription() ?>">
-  <i class="uil uil-edit"></i>
+<button onclick="openEditTaskModal(this)" class="ml-2 rounded-md bg-amber-600 px-3 py-2 text-white font-semibold shadow-sm hover:bg-amber-500 disabled:bg-amber-500 disabled:text-amber-200" task-id="<?= $task->getID() ?>" task-title="<?= $task->getTitle() ?>" task-description="<?= $task->getDescription() ?>">
+  <i class="uil uil-edit"></i> Edit
 </button>
