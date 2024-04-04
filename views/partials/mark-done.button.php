aec576dca8db98a002b66dd884c925788a4207c8
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-04-04 18:52:38 -0300

    feat: add responsive design to button components

--- views/partials/mark-done.button.php
+++ views/partials/mark-done.button.php
@@ -2,7 +2,7 @@
   <input type="hidden" name="type" value="complete">
   <input type="hidden" name="id" value="<?= $task->getID() ?>">
   <input type="hidden" name="completed" value="1">
-  <button type="submit" class="rounded-md bg-amber-600 px-3 py-2 text-white font-semibold shadow-sm hover:bg-amber-500 disabled:bg-amber-500 disabled:text-amber-200">
+  <button type="submit" class="rounded-md bg-amber-600 px-3 py-2 text-white text-sm font-semibold shadow-sm sm:text-base hover:bg-amber-500 disabled:bg-amber-500 disabled:text-amber-200">
     <i class="uil uil-check-circle"></i> Mark as done
   </button>
 </form>
