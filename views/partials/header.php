4fcc68c6f770c91c25e4fd8fb611ff96a6354946
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-04-11 22:30:24 -0300

    refactor: update header partial
    This commit updates the header partial structure\n
    to fit the future login system buttons.

--- views/partials/header.php
+++ views/partials/header.php
@@ -11,66 +11,43 @@
 </head>
 
 <body class="h-full">
-  <div class="min-h-full">
-    <nav class="bg-gray-800 fixed top-0 w-full z-10" id="nav-menu">
-      <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div class="flex h-16 items-center justify-between">
-          <div class="flex items-center">
-            <div class="flex-shrink-0">
-              <a href="/" class="flex items-center text-white font-bold sm:text-lg">
-                <img class="h-8 w-8 sm:h-10 sm:w-10" src="/assets/img/check.svg" alt="JATL logo"> JATL
-              </a>
-            </div>
+  <nav class="bg-gray-800 fixed top-0 h-16 w-full z-10" id="nav-menu">
+    <div class="max-w-7xl mx-auto h-full px-4 flex items-center justify-between sm:px-6 lg:px-8">
+      <div class="flex items-center justify-between">
+        <div>
+          <a href="/" class="flex items-center text-white font-bold sm:text-lg">
+            <img class="h-8 w-8 sm:h-10 sm:w-10" src="/assets/img/check.svg" alt="JATL logo"> JATL
+          </a>
+        </div>
 
-            <div class="hidden md:block">
-              <div class="ml-10 flex items-baseline space-x-4">
-                <a href="/" class="<?= checkUrl('/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium md:text-base" aria-current="page">
-                  To-do
-                </a>
+        <div class="hidden ml-10 flex items-center space-x-4 md:block ">
+          <a href="/" class="<?= checkUrl('/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium md:text-base" aria-current="page">To-do</a>
 
-                <a href="/done" class="<?= checkUrl('/done') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium md:text-base">
-                  Done
-                </a>
+          <a href="/done" class="<?= checkUrl('/done') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium md:text-base">Done</a>
 
-                <a href="/about" class="<?= checkUrl('/about') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium md:text-base">
-                  About us
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div class="-mr-2 flex md:hidden">
-            <!-- Mobile menu button -->
-            <button onclick="mobileMenuToggle()" type="button" class="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" aria-controls="mobile-menu" aria-expanded="false">
-              <!-- Menu open: "hidden", Menu closed: "block" -->
-              <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" id="mobile-menu-open">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-              </svg>
-              <!-- Menu open: "block", Menu closed: "hidden" -->
-              <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" id="mobile-menu-close">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-              </svg>
-            </button>
-          </div>
+          <a href="/about" class="<?= checkUrl('/about') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium md:text-base">About us</a>
         </div>
       </div>
 
-      <!-- Mobile menu, show/hide based on menu state. -->
-      <div class="hidden md:hidden" id="mobile-menu">
-        <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-          <a href="/" class="<?= checkUrl('/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> block rounded-md px-3 py-2 text-base font-medium" aria-current="page">
-            To-do
-          </a>
-
-          <a href="/done" class="<?= checkUrl('/done') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> block rounded-md px-3 py-2 text-base font-medium">
-            Done
-          </a>
-
-          <a href="/about" class="<?= checkUrl('/about') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> block rounded-md px-3 py-2 text-base font-medium">
-            About us
-          </a>
-        </div>
+      <!-- Mobile menu button -->
+      <div class="md:hidden">
+        <button onclick="mobileMenuToggle()" type="button" class="rounded-md bg-gray-800 px-2 py-1 text-2xl text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+          <i class="uil uil-bars" id="mobile-menu-open"></i>
+          <i class="uil uil-times hidden" id="mobile-menu-close"></i>
+        </button>
       </div>
-    </nav>
+    </div>
 
-    <?php require_once(__DIR__ . '/heading.php') ?>
+    <!-- Mobile menu, show/hide based on menu state. -->
+    <div class="hidden md:hidden bg-gray-800 rounded-b-lg" id="mobile-menu">
+      <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
+        <a href="/" class="<?= checkUrl('/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> block rounded-md px-3 py-2 text-base font-medium" aria-current="page">To-do</a>
+
+        <a href="/done" class="<?= checkUrl('/done') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> block rounded-md px-3 py-2 text-base font-medium">Done</a>
+
+        <a href="/about" class="<?= checkUrl('/about') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> block rounded-md px-3 py-2 text-base font-medium">About us</a>
+      </div>
+    </div>
+  </nav>
+
+  <?php require_once(__DIR__ . '/heading.php') ?>
