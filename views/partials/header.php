47194921c319f6917be8f38aa200dd1c0f6f2d96
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-04-13 00:00:36 -0300

    fix: fix register button in bigger mobile screens
    This commit fixes the register button in bigger mobile screens.
    The button was being show in a strange way.

--- views/partials/header.php
+++ views/partials/header.php
@@ -64,10 +64,10 @@
             <button>Login</button>
           </a>
 
-          <hr class="border-gray-700 w-3/4 my-2">
+          <hr class="border-gray-700 w-3/4 my-2 sm:hidden">
 
-          <a href="/register" class="w-11/12 justify-center rounded-md bg-white px-3 py-2 text-sm text-center font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-            Register
+          <a href="/register" class="w-11/12 justify-center rounded-md bg-white px-3 py-2 text-sm text-center font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:w-2/4">
+            <button>Register</button>
           </a>
         </div>
       </div>
