e59312e5cc607500384743ca7a4bc62867cb9bae
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-04-14 20:58:17 -0300

    refactor: make "New task" button a partial

---
+++ views/partials/button.new-task.php
@@ -0,0 +1,5 @@
+<div class="fixed bottom-2 right-4 sm:static">
+  <button type="button" onclick="openModal(this)" modal-id="new-task-modal" class="bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm rounded-md hover:bg-indigo-500 md:text-base">
+    <i class="uil uil-plus"></i> New task
+  </button>
+</div>
