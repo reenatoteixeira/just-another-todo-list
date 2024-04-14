24d7f77f7d30c3d5d93cd4b9bcbb4e7579058b3e
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-04-14 20:58:42 -0300

    refactor: request "New task" button partial

--- views/partials/template.heading.php
+++ views/partials/template.heading.php
@@ -6,13 +6,9 @@
       </h1>
     </div>
 
-    <?php if ($heading === 'To-do') : ?>
-      <div class="fixed bottom-2 right-4 sm:static">
-        <button type="button" onclick="openModal(this)" modal-id="new-task-modal" class="bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm rounded-md hover:bg-indigo-500 md:text-base">
-          <i class="uil uil-plus"></i> New task
-        </button>
-      </div>
-    <?php endif; ?>
+    <?php if ($heading === 'To-do') {
+      require_once(__DIR__ . '/button.new-task.php');
+    }; ?>
   </div>
 </header>
 
