e6cc75151fbfcbb24bfa9bce9718196023dce828
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-04-17 23:53:02 -0300

    feat: update "404" page padding top

--- views/view.404.php
+++ views/view.404.php
@@ -1,5 +1,5 @@
 <?php require_once(__DIR__ . '/partials/template.header.php'); ?>
-<div class="text-center mt-32">
+<div class="text-center pt-60">
   <p class="text-base font-semibold text-indigo-600">404</p>
   <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
   <p class="mt-6 text-base leading-7 text-gray-600 px-8">Sorry, we couldn’t find the page you’re looking for.</p>
