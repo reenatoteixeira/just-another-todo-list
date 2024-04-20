181e82f85056e5354049474d7cc45a329bdb1733
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-04-20 11:30:42 -0300

    feat: add "Login" page
    This commit adds a new "Login" page to the application.

--- views/view.login.php
+++ views/view.login.php
@@ -16,9 +16,9 @@
       <div>
         <div class="flex items-center justify-between">
           <label for="password" class="block text-sm font-medium leading-6 text-gray-900 md:text-base">Password</label>
-          <div class="text-sm">
+          <!-- <div class="text-sm">
             <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
-          </div>
+          </div> -->
         </div>
         <div class="mt-2">
           <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-base sm:leading-6">
