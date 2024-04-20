f4ce1623bdacc72afd798b8ee478e35903c35b26
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-04-20 17:00:26 -0300

    chore: update database example
    This commit updates database example file to match the current database schema.

--- config/database.example.sql
+++ config/database.example.sql
@@ -4,11 +4,22 @@ CREATE DATABASE database_name;
 -- Select your database (change 'database_name' to your database name):
 USE DATABASE database_name;
 
--- Create your own table (change 'table_name' to the desired name):
-CREATE TABLE table_name (
+-- Create your own tasks table:
+CREATE TABLE tasks (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(125) NOT NULL,
     description VARCHAR(255),
+    user_id INT NOT NULL,
     completed INT(1) DEFAULT 0,
     deleted INT(1) DEFAULT 0
+);
+
+-- Create your own users table:
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(200) NOT NULL,
+    token VARCHAR(200) NOT NULL
 );
