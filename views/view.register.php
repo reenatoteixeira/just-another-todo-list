412086c28193b3e9ee1154af6fa21d17f1f80999
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-04-20 13:43:42 -0300

    feat: add "Register" page
    This commit adds the "Register" page. This page is accessible to
    everyone and allows users to create an account on the website.

--- views/view.register.php
+++ views/view.register.php
@@ -1,3 +1,59 @@
 <?php require_once(__DIR__ . '/partials/template.header.php'); ?>
+<div class="flex flex-col justify-center">
+  <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+    <h2 class="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Create new account</h2>
+  </div>
 
+  <div class="bg-white mt-8 p-6 rounded-lg shadow-lg sm:mx-auto sm:w-full sm:max-w-lg">
+    <form class="space-y-6" action="/config/process.users.php" method="POST">
+      <div>
+        <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900 md:text-base">First name</label>
+        <div class="mt-2">
+          <input id="first-name" name="first-name" type="text" maxlength="100" placeholder="John" required class="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-base sm:leading-6">
+        </div>
+      </div>
+
+      <div>
+        <label for="last-name" class="block text-sm font-medium leading-6 text-gray-900 md:text-base">Last name</label>
+        <div class="mt-2">
+          <input id="last-name" name="last-name" type="text" maxlength="100" placeholder="Doe" required class="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-base sm:leading-6">
+        </div>
+      </div>
+
+      <div>
+        <label for="email" class="block text-sm font-medium leading-6 text-gray-900 md:text-base">Email</label>
+        <div class="mt-2">
+          <input id="email" name="email" type="email" autocomplete="email" maxlength="100" placeholder="johndoe@email.com" required class="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-base sm:leading-6">
+        </div>
+      </div>
+
+      <div>
+        <label for="password" class="block text-sm font-medium leading-6 text-gray-900 md:text-base">Password</label>
+        <div class="mt-2">
+          <input id="password" name="password" type="password" maxlength="25" required class="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-base sm:leading-6">
+        </div>
+      </div>
+
+      <div>
+        <div class="flex items-center justify-between">
+          <label for="check-password" class="block text-sm font-medium leading-6 text-gray-900 md:text-base">Confirm password</label>
+        </div>
+        <div class="mt-2">
+          <input id="check-password" name="check-password" type="password" required class="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-base sm:leading-6">
+        </div>
+      </div>
+
+      <div>
+        <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 md:text-base">
+          Create account
+        </button>
+      </div>
+    </form>
+
+    <p class="mt-6 text-center text-sm text-gray-500">
+      Already have an account?
+      <a href="/login" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Login here</a>
+    </p>
+  </div>
+</div>
 <?php require_once(__DIR__ . '/partials/template.footer.php'); ?>
