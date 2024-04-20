576f21964aee1f8b2f0a282de606a30f415b4864
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-04-20 17:10:00 -0300

    chore: add `User` class

---
+++ models/User.php
@@ -0,0 +1,70 @@
+<?php 
+class User
+{
+  private $id;
+  private $firstName;
+  private $lastName;
+  private $email;
+  private $password;
+  private $token;
+
+  public function getId(): int
+  {
+    return $this->id;
+  }
+
+  public function setId(int $id): void
+  {
+    $this->id = $id;
+  }
+
+  public function getFirstName(): string
+  {
+    return $this->firstName;
+  }
+
+  public function setFirstName(string $firstName): void
+  {
+    $this->firstName = $firstName;
+  }
+
+  public function getLastName(): string
+  {
+    return $this->lastName;
+  }
+
+  public function setLastName(string $lastName): void
+  {
+    $this->lastName = $lastName;
+  }
+
+  public function getEmail(): string
+  {
+    return $this->email;
+  }
+
+  public function setEmail(string $email): void
+  {
+    $this->email = $email;
+  }
+
+  public function getPassword(): string
+  {
+    return $this->password;
+  }
+
+  public function setPassword(string $password): void
+  {
+    $this->password = $password;
+  }
+
+  public function getToken(): string
+  {
+    return $this->token;
+  }
+
+  public function setToken(string $token): void
+  {
+    $this->token = $token;
+  }
+}
