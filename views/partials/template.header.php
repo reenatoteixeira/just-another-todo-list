9f54ef198884173f5265972f8fe957378194696a
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-04-22 23:12:12 -0300

    wip: add session message to header

--- views/partials/template.header.php
+++ views/partials/template.header.php
@@ -1,3 +1,15 @@
+<?php
+require_once(__DIR__ . '/../../config/database.php');
+require_once(__DIR__ . '/../../models/Message.php');
+
+$message = new Message();
+$flashMessage = $message->getMessage();
+
+if (!empty($flashMessage['message'])) {
+  $message->clearMessage();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en" class="h-full bg-gray-100">
 
@@ -11,3 +23,14 @@
 </head>
 
 <body class="h-full">
+  <?php if (!empty($flashMessage['message'])) : ?>
+    <div class="<?= checkFlashMsgType($flashMessage['type']) ?> flex items-center justify-between rounded-lg p-4 mt-44 mb-4 m-auto shadow-md text-sm max-w-xl sm:text-base" id="flash-msg">
+      <h2 class="flex items-center space-x-2">
+        <i class="<?= checkFlashMsgIcon($flashMessage['type']) ?> uil text-xl"></i>
+        <span><?= $flashMessage['message'] ?></span>
+      </h2>
+      <button onclick="closeFlashMsg()">
+        <i class="uil uil-times text-xl"></i>
+      </button>
+    </div>
+  <?php endif; ?>
