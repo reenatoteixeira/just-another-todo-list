c3b01c4c1773c14dfcf5dd5c1252c65c4b5c899f
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-04-22 23:10:02 -0300

    feat: add flash message functions

--- config/functions.php
+++ config/functions.php
@@ -4,6 +4,28 @@ function checkUrl(string $url): bool
   return $_SERVER['REQUEST_URI'] === $url;
 }
 
+function checkFlashMsgType($type): string
+{
+  if ($type === 'success') {
+    return 'bg-green-100 text-green-600';
+  }
+
+  if ($type === 'error') {
+    return 'bg-red-100 text-red-600';
+  }
+}
+
+function checkFlashMsgIcon($type): string
+{
+  if ($type === 'success') {
+    return 'uil-check-circle';
+  }
+
+  if ($type === 'error') {
+    return 'uil-times-circle';
+  }
+}
+
 function abort(int $code = 404): void
 {
   http_response_code($code);
