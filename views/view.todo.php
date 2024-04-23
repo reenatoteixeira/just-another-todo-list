6aed3a8689eeacb4f65adb913b531c21551fc893
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-04-22 23:09:44 -0300

    feat: remove warning message

--- views/view.todo.php
+++ views/view.todo.php
@@ -1,43 +1,14 @@
 <?php
-require_once(__DIR__ . '/../config/database.php');
-require_once(__DIR__ . '/../models/dao/TaskDAO.php');
 require_once(__DIR__ . '/../views/partials/template.header.php');
 require_once(__DIR__ . '/../views/partials/template.navbar.php');
 require_once(__DIR__ . '/../views/partials/template.heading.php');
+require_once(__DIR__ . '/../models/dao/TaskDAO.php');
 
 $taskDAO = new TaskDAO($pdo);
 $tasks = $taskDAO->read(0, 0);
 ?>
 
 <section class="mt-44">
-  <div class="flex flex-col bg-blue-100 text-blue-600 rounded-lg p-4 mb-4 shadow-md text-sm sm:text-base" id="warning-msg">
-    <div class="flex items-center justify-between items-center">
-      <h2><i class="uil uil-info-circle text-xl"></i> <strong>This is a test environment.</strong></h2>
-      <button onclick="closeWarning()">
-        <i class="uil uil-times text-xl"></i>
-      </button>
-    </div>
-
-    <hr class="border-blue-200 my-2 w-full">
-    <p> Here's what you can do from here:</p>
-    <ul class="list-disc ml-6 my-2">
-      <li>
-        <b>Create</b> a new task by clicking on "New task"
-      </li>
-      <li>
-        <b>Delete</b> any task (including other people's) by clicking on "Delete"
-      </li>
-      <li>
-        <b>Edit</b> any task (including other people's) by clicking on "Edit"
-      </li>
-      <li>
-        <b>Finish</b> any task (including other people's) by clicking on "Mark as done"
-      </li>
-    </ul>
-
-    <p>We're working on future improvements, <a href="https://github.com/reenatoteixeira/just-another-todo-list/tree/stage" target="_blank" class="underline">click here</a> to check our GitHub repository. Thanks!</p>
-  </div>
-
   <?php if (count($tasks) === 0) : ?>
     <div class="flex items-center justify-center">
       <div class="border-gray-300 border-2 rounded-lg border-dashed w-full py-32 px-4">
