449b93591231bbe61735ebdcf85d1a505349ca5a
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-04-24 23:47:18 -0300

    docs: update database example file

--- config/database.example.sql
+++ config/database.example.sql
@@ -4,16 +4,6 @@ CREATE DATABASE database_name;
 -- Select your database (change 'database_name' to your database name):
 USE DATABASE database_name;
 
--- Create your own tasks table:
-CREATE TABLE tasks (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(125) NOT NULL,
-    description VARCHAR(255),
-    user_id INT NOT NULL,
-    completed INT(1) DEFAULT 0,
-    deleted INT(1) DEFAULT 0
-);
-
 -- Create your own users table:
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -22,4 +12,15 @@ CREATE TABLE users (
     email VARCHAR(100) NOT NULL,
     password VARCHAR(200) NOT NULL,
     token VARCHAR(200) NOT NULL
+);
+
+-- Create your own tasks table:
+CREATE TABLE tasks (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(125) NOT NULL,
+    description VARCHAR(255),
+    user_id INT,
+    completed INT(1) DEFAULT 0,
+    deleted INT(1) DEFAULT 0,
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
