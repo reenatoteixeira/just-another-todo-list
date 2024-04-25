3582e9d58671378bd2581d02e3c89feba9c871f7
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-04-24 22:48:56 -0300

    feat: make "Todo" view private

--- views/view.todo.php
+++ views/view.todo.php
@@ -3,9 +3,13 @@ require_once(__DIR__ . '/../views/partials/template.header.php');
 require_once(__DIR__ . '/../views/partials/template.navbar.php');
 require_once(__DIR__ . '/../views/partials/template.heading.php');
 require_once(__DIR__ . '/../models/dao/TaskDAO.php');
+require_once(__DIR__ . '/../models/dao/UserDAO.php');
 
 $taskDAO = new TaskDAO($pdo);
 $tasks = $taskDAO->read(0, 0);
+
+$userDAO = new UserDAO($pdo);
+$userData = $userDAO->verifyToken(true);
 ?>
 
 <section class="mt-44">
