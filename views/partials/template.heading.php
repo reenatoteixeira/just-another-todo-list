79c93f66e090057a73668bdd9f027cf55dea8462
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-04-30 15:24:24 -0300

    refactor: update `template.heading.php` layout

--- views/partials/template.heading.php
+++ views/partials/template.heading.php
@@ -1,15 +1,14 @@
-<header class="bg-white shadow fixed top-16 w-full z-0">
-  <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8 flex justify-between">
+<div class="bg-white w-full shadow fixed top-0">
+  <div class="flex justify-between max-w-7xl mx-auto pt-20 mt-2 pb-4 px-4 sm:px-6 lg:px-8">
     <div>
-      <h1 class="text-3xl font-bold tracking-tight text-gray-900 md:text-4xl">
+      <h1 class="text-2xl font-bold tracking-tight text-gray-900">
         <?= $heading ?>
       </h1>
     </div>
 
-    <?php if ($heading === 'To-do') {
+
+    <?php if ($heading === "To-do") {
       require_once(__DIR__ . '/button.new-task.php');
     }; ?>
   </div>
-</header>
-
-<main class="mx-auto max-w-7xl pb-12 px-4 sm:px-6 lg:px-8">
+</div>
