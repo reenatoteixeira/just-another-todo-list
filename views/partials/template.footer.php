b2b994f99fcb92add0f1f188c78bcedb6c339596
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-04-30 15:26:09 -0300

    feat: update `template.footer.php` layout

--- views/partials/template.footer.php
+++ views/partials/template.footer.php
@@ -3,8 +3,16 @@ require_once(__DIR__ . '/modal.new-task.php');
 require_once(__DIR__ . '/modal.edit-task.php');
 require_once(__DIR__ . '/modal.delete-task.php');
 ?>
+
 </main>
-</div>
+
+<footer class="absolute bottom-0 w-full h-20 flex items-center justify-center">
+  <div class="text-center text-slate-400">
+    <p>JATL - Just Another Todo List</p>
+    <p>&copy; <?= date("Y") ?>, Renato Teixeira</p>
+  </div>
+</footer>
+
 <script src="./assets/js/main.js"></script>
 </body>
 
